184096f034072268e62f483e226c9c38390b4c7e
abhash-er <abhashkumar.te17@bmsce.ac.in>
2020-12-08 23:30:33 +0530

    Demo Structure-xtensor

---
+++ build/CMakeFiles/Autoencoders.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Autoencoders.
+# This may be replaced when dependencies are built.
---
+++ src/deep learning/activation function/Sigmoid.cpp
@@ -0,0 +1,11 @@
+#include "Sigmoid.h"
+#include "xtensor/xmath.hpp"
+
+xt::xarray<double> Sigmoid::sigmoid(xt::xarray<double> arr){
+    return 1.0/(1.0+xt::exp(arr));
+}
+
+xt::xarray<double> Sigmoid::gradient(xt::xarray<double> arr){
+    xt::xarray<double> a = Sigmoid::sigmoid(arr);
+    return a*(1.0-a);
+}
